bc007dec3cbeafd687a20c09fbf3d768d7eec025
nikitadybaa <nikitadyba@mail.ru>
2017-03-30 21:47:51 +0300

    fixed bug with border, added new meteorits,new todo

--- config.py
+++ config.py
@@ -4,13 +4,14 @@ line_now = 2
 height = 10
 width = 30
 border = list(width * "#")
-line_with_spacecar = list("#"+">00>"+(width - 6 )*" " + "#")
-empty_line = list("#" + (width -2)* " " + "#")
+line_with_spacecar = list(">00>"+(width - 4)*" " )
+empty_line = list( (width)* " " )
 i_for_meteorit  = 0
 hp = 2
 spaces_level  = 4
 line_without_spacecar = []
 for i in range(height):
 	line_without_spacecar.append(empty_line)
+
 lines = []
 
--- game.py
+++ game.py
@@ -1,18 +1,6 @@
 import os,time,sys,threading
 from random import *
 from config import *
-"""def lines_do(lines_without_spacecar):
-	global liness
-	liness = []
-	for i in range(line_now -1):
-		if lines_without_spacecar:
-			liness.append(lines_without_spacecar[0])
-			lines_without_spacecar[i].pop(0)
-	liness.append(line_with_spacecar)
-	for i in range(height - line_now):
-		if lines_without_spacecar:
-			liness.append(lines_without_spacecar[0])
-			lines_without_spacecar[i].pop(0)"""
 try:
     from msvcrt import getch
 except ImportError:
@@ -29,17 +17,15 @@ except ImportError:
 def keypress():
 	global char
 	while 1:
-		char = getch()
-		
+		char = getch()	
 def to_str(my_list):
-	return "".join(my_list) + "\r"
+	return "#" + "".join(my_list) + "#\r"
 def clear_screen():
 	try:
 		os.system('clear')
 	except:
 		os.system('cls')
 def paint_game():
-	global k 
 	k = 0
 	clear_screen()
 	print(str(hp) + "\r")
@@ -51,48 +37,39 @@ def paint_game():
 	for i in range(height-line_now):
 		k += 1
 		print(to_str(line_without_spacecar[k]))
-		
 	print(to_str(border))
 def new_meteorit():
 	global i_for_meteorit
 	i_for_meteorit += 1
 	if  i_for_meteorit % (spaces_level):
 		for i in range(len(line_without_spacecar)):
-			line_without_spacecar[i].pop(1)
-			line_without_spacecar[i].insert(width- 2," ")
+			line_without_spacecar[i]  = line_without_spacecar[i][1:width]+[" "]
 	else:
-		"""z = choice(line_without_spacecar)
-		z.pop(1)
-		z.insert(width- 2,"*")"""
-		z = randint(0,height)
+		z = randint(0,height - 1)
 		o = 0
 		for i in range(z - 1):
-			"""line_without_spacecar[i].pop(1)
-			line_without_spacecar[i].insert(width- 2," ")
-			o += 1"""
-			line_without_spacecar[o]  = line_without_spacecar[o][1:]+[" "]
+			line_without_spacecar[o]  = line_without_spacecar[o][1:width]+[" "]
 			o += 1
-		"""line_without_spacecar[z].pop(1)
-		line_without_spacecar[z].insert(width- 2,"*")"""
-		line_without_spacecar[z] = line_without_spacecar[z][1:]+["*"]
+		line_without_spacecar[z] = line_without_spacecar[z][1:width]+["*"]
 		for i in range(height - z):
-			"""line_without_spacecar[o].pop(1)
-			line_without_spacecar[o].insert(width- 2," ")"""
-			
-			line_without_spacecar[o]  = line_without_spacecar[o][1:]+[" "]
+			line_without_spacecar[o]  = line_without_spacecar[o][1:width]+[" "]
 			o += 1
 def proverka_na_simvol():
 	global hp,line_now,char
 	if char is not None:
 			if char == 'w' and line_now != 1:
-				if "*" in line_without_spacecar[line_now -1][1:6] :
+				if "*" in line_without_spacecar[line_now -2][1:6] :
+					#to do hp * count *
+					# to do "*  "-->"    "
+					#to do fix bug with border
+					
 					hp -= 2
 					line_now -= 1
 				else:
 					line_now -= 1
 				char = None
 			elif char == 's' and line_now != height:
-				if "*" in line_without_spacecar[line_now + 1][1:6]:
+				if "*" in line_without_spacecar[line_now][1:6]:
 					hp -= 2
 					line_now += 1
 				else:	
@@ -103,11 +80,7 @@ def proverka_na_simvol():
 if __name__ =='__main__':
 	th = threading.Thread(target=keypress).start()
 	while 1:
-
-		#lines_do(lines_without_spacecars)
 		proverka_na_simvol()
 		new_meteorit()
 		paint_game()
 		time.sleep(0.25)
-
-
