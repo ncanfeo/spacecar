c2c1190806fa62969193bede61ba2cccb2fb038f
nikitadybaa <nikitadyba@mail.ru>
2017-04-02 19:22:02 +0300

    refactoring

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 Red = '\033[91m'
 Green = '\033[92m'
-Blue = '\033[94m'
+NavyBlue = '\033[94m'
 Cyan = '\033[96m'
 White = '\033[97m'
 Yellow = '\033[93m'
@@ -15,20 +15,22 @@ line_now = 2
 height = 10
 width = 30
 border = list(width * "#")
-line_with_spacecar = list("{1}>{2}{0}00{2}{1}>{2}".format(Blue,Red,End)+(width - 4)*" " )
-empty_line = list( (width)* " " )
-i_for_meteorit  = 0
+line_with_spacecar = list("{1}>{2}{0}00{2}{1}>{2}".format(NavyBlue, Red, End) +
+                          (width - 4) * " ")
+empty_line = list((width) * " ")
+i_for_meteorit = 0
 hp = 10
-spaces_level  =  1
+spaces_level = 1
 line_without_spacecar = []
 for i in range(height):
-	line_without_spacecar.append(empty_line)
+    line_without_spacecar.append(empty_line)
 lines = []
 gameover = '''{0}   _____          __  __ ______    ______      ________ _____    _\r
   / ____|   /\   |  \/  |  ____|  / __ \ \    / /  ____|  __ \  | |\r
  | |  __   /  \  | \  / | |__    | |  | \ \  / /| |__  | |__) | | |\r
  | | |_ | / /\ \ | |\/| |  __|   | |  | |\ \/ / |  __| |  _  /  | |\r
  | |__| |/ ____ \| |  | | |____  | |__| | \  /  | |____| | \ \  |_|\r
-  \_____/_/    \_\_|  |_|______|  \____/   \/   |______|_|  \_\ (_)\r{1}'''.format(Yellow,End)
-delay  = 0.25
+  \_____/_/    \_\_|  |_|______|  \____/   \/   |______|_|  \_\ (_)\r{1}'''.format(
+    Yellow, End)
+delay = 0.25
 znak = "@"
--- game.py
+++ game.py
@@ -1,9 +1,10 @@
-import os,time,sys,threading
+import os, time, sys, threading
 from random import *
 from config import *
 try:
     from msvcrt import getch
 except ImportError:
+
     def getch():
         import sys, tty, termios
         fd = sys.stdin.fileno()
@@ -14,88 +15,110 @@ except ImportError:
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
+
+
 def keypress():
-	global char
-	while 1:
-		char = getch()	
+    global char
+    while 1:
+        char = getch()
+
+
 def to_str(my_list):
-	return "#" + "".join(my_list) + "#\r"
+    return "#" + "".join(my_list) + "#\r"
+
+
 def clear_screen():
-	try:
-		os.system('clear')
-	except:
-		os.system('cls')
+    try:
+        os.system('clear')
+    except:
+        os.system('cls')
+
+
 def paint_game():
-	k = 0
-	clear_screen()
-	print("Health: " + str(hp) + "\r")
-	print(to_str(border))
-	for i in range(height - 1 -(height-line_now)):
-		print(to_str(line_without_spacecar[i]))
-		k += 1
-	print(to_str(line_with_spacecar))
-	for i in range(height-line_now):
-		print(to_str(line_without_spacecar[k]))
-		k += 1
-	print(to_str(border))
-	print("")
+    k = 0
+    clear_screen()
+    print("Health: " + str(hp) + "\r")
+    print(to_str(border))
+    for i in range(height - 1 - (height - line_now)):
+        print(to_str(line_without_spacecar[i]))
+        k += 1
+    print(to_str(line_with_spacecar))
+    for i in range(height - line_now):
+        print(to_str(line_without_spacecar[k]))
+        k += 1
+    print(to_str(border))
+    print("")
+
+
 def new_meteorit():
-	global i_for_meteorit
-	i_for_meteorit += 1
-	if  i_for_meteorit % (spaces_level):
-		for i in range(len(line_without_spacecar)):
-			line_without_spacecar[i]  = line_without_spacecar[i][1:width]+[" "]
-	else:
-		z = randint(0,height - 1)
-		o = 0
-		for i in range(z - 1):
-			line_without_spacecar[o]  = line_without_spacecar[o][1:width]+[" "]
-			o += 1
-		line_without_spacecar[z] = line_without_spacecar[z][1:width]+["\033[96m{}\033[0m".format(znak)]
-		for i in range(height - z):
-			line_without_spacecar[o]  = line_without_spacecar[o][1:width]+[" "]
-			o += 1
+    global i_for_meteorit
+    i_for_meteorit += 1
+    if i_for_meteorit % (spaces_level):
+        for i in range(len(line_without_spacecar)):
+            line_without_spacecar[
+                i] = line_without_spacecar[i][1:width] + [" "]
+    else:
+        z = randint(0, height - 1)
+        o = 0
+        for i in range(z - 1):
+            line_without_spacecar[
+                o] = line_without_spacecar[o][1:width] + [" "]
+            o += 1
+        line_without_spacecar[z] = line_without_spacecar[z][1:width] + [
+            "{}{}{}".format(Cyan, znak, End)
+        ]
+        for i in range(height - z):
+            line_without_spacecar[
+                o] = line_without_spacecar[o][1:width] + [" "]
+            o += 1
+
+
 def proverka_na_simvol():
-	global hp,line_now,char
-	if char is not None:
-				
-			if char.lower() == 'w' and line_now != 1:
-				if "\033[96m{}\033[0m".format(znak) in line_without_spacecar[line_now -2][1:5] :
-					c = 0
-					for i in line_without_spacecar[line_now -2][1:5]:
-						if i == "\033[96m{}\033[0m".format(znak):
-							c += 1
-					hp -= 2 * c
-					line_without_spacecar[line_now -2][1:5] = " " * 5
-					line_now -= 1
-				else:
-					line_now -= 1
-				char = None
-			elif char.lower() == 's' and line_now != height:
-				if "\033[96m{}\033[0m".format(znak) in line_without_spacecar[line_now -1][1:5]:
-					c = 0
-					for i in line_without_spacecar[line_now -1][1:5]:
-						if i == "\033[96m{}\033[0m".format(znak):
-							c += 1
-					hp -= 2 * c
-					line_without_spacecar[line_now -1][1:5] = " " * 5
-					line_now += 1
-				else:	
-					line_now += 1
-				char = None
-			elif char == " ":
-				pass 
-			elif char == 'q':
-				sys.exit()
-			
-if __name__ =='__main__':
-	threading.Thread(target=keypress).start()
-	while 1:
-		if hp < 1:
-			clear_screen()
-			print(gameover)
-			sys.exit()
-		proverka_na_simvol()
-		new_meteorit()
-		paint_game()
-		time.sleep(delay)
+    global hp, line_now, char
+    if char is not None:
+
+        if char.lower() == 'w' and line_now != 1:
+            if "{}{}{}".format(Cyan, znak,
+                               End) in line_without_spacecar[line_now -
+                                                             2][1:5]:
+                c = 0
+                for i in line_without_spacecar[line_now - 2][1:5]:
+                    if i == "{}{}{}".format(Cyan, znak, End):
+                        c += 1
+                hp -= 2 * c
+                line_without_spacecar[line_now - 2][1:5] = " " * 5
+                line_now -= 1
+            else:
+                line_now -= 1
+            char = None
+        elif char.lower() == 's' and line_now != height:
+            if "{}{}{}".format(Cyan, znak,
+                               End) in line_without_spacecar[line_now -
+                                                             1][1:5]:
+                c = 0
+                for i in line_without_spacecar[line_now - 1][1:5]:
+                    if i == "{}{}{}".format(Cyan, znak, End):
+                        c += 1
+                hp -= 2 * c
+                line_without_spacecar[line_now - 1][1:5] = " " * 5
+                line_now += 1
+            else:
+                line_now += 1
+            char = None
+        elif char == " ":
+            pass
+        elif char == 'q':
+            sys.exit()
+
+
+if __name__ == '__main__':
+    threading.Thread(target=keypress).start()
+    while 1:
+        if hp < 1:
+            clear_screen()
+            print(gameover)
+            sys.exit()
+        proverka_na_simvol()
+        new_meteorit()
+        paint_game()
+        time.sleep(delay)
